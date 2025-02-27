750fbc8316e80bf5529723b363906a9e86f02156
Jack Lavigne <jacklavigne00@gmail.com>
2025-02-27 15:06:45 +0100

    fix: clone field

--- msfs/src/network.rs
+++ msfs/src/network.rs
@@ -72,8 +72,9 @@ impl<'a> NetworkRequestBuilder<'a> {
         ) -> sys::FsNetworkRequestId,
     ) -> Option<NetworkRequest> {
         // SAFETY: we need a *mut i8 for the FsNetworkHttpRequestParam struct but this should be fine.
-        let raw_post_field =
-            post_field.map_or(ptr::null_mut(), |f| f.as_c_str().as_ptr() as *mut i8);
+        let raw_post_field = post_field
+            .clone()
+            .map_or(ptr::null_mut(), |f| f.as_c_str().as_ptr() as *mut i8);
         // SAFETY: Because the struct in the C code is not defined as const char* we need to cast
         // the *const into *mut which should be safe because the function should not change it anyway
         let mut headers = self
