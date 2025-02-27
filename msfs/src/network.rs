7288be71a0c69134c7f3af538c5a44b28d79068b
Jack Lavigne <jacklavigne00@gmail.com>
2025-02-27 15:03:28 +0100

    try data len from post field

--- msfs/src/network.rs
+++ msfs/src/network.rs
@@ -81,7 +81,11 @@ impl<'a> NetworkRequestBuilder<'a> {
             .iter_mut()
             .map(|h| h.as_ptr() as *mut i8)
             .collect::<Vec<_>>();
-        let data_len = self.data.as_ref().map_or(0, |d| d.len());
+        let data_len = if let Some(post_field) = post_field {
+            post_field.as_bytes().len()
+        } else {
+            self.data.as_ref().map_or(0, |d| d.len())
+        };
         let mut params = sys::FsNetworkHttpRequestParam {
             postField: raw_post_field,
             headerOptions: headers.as_mut_ptr(),
